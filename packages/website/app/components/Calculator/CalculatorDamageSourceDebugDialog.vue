34741bdf8b4682d9abd24c15c8f16434cad0c1ba
asasinmode <skin9470@gmail.com>
2026-05-07 18:27:27 +0200

    add stats to debug dialog

--- packages/website/app/components/Calculator/CalculatorDamageSourceDebugDialog.vue
+++ packages/website/app/components/Calculator/CalculatorDamageSourceDebugDialog.vue
@@ -12,9 +12,56 @@ defineExpose({
 
 <template>
 	<VDialog
-		id="damage-source-debug"
+		id="dialog-damage-source-debug"
 		ref="vDialog"
 	>
-		debug {{ damageSource?.id }}
+		<h1>debug <span>{{ damageSource?.champion.value?.name }} ({{ damageSource?.id }})</span></h1>
+		<h2>stats</h2>
+		<div>
+			<details v-for="(stats, statGroup) in damageSource?.stats.value" :key="statGroup" open>
+				<summary>{{ statGroup }}</summary>
+				<div>
+					<code class="whitespace-pre">
+						{{ JSON.stringify(stats, null, 2) }}
+					</code>
+				</div>
+			</details>
+		</div>
 	</VDialog>
 </template>
+
+<style>
+@layer components {
+	#dialog-damage-source-debug {
+		--at-apply: 'bg-mauve-900 b b-[--ui-btn-border-clr] shadow-lg of-x-auto p-4';
+
+		> h1 {
+			--at-apply: 'text-xl mb-3';
+
+			> span {
+				--at-apply: 'font-500';
+			}
+		}
+
+		> h2 {
+			--at-apply: 'text-lg mb-1';
+		}
+
+		> div {
+			--at-apply: 'flex divide-x divide-neutral-500';
+
+			> details {
+				--at-apply: 'me-4 pe-4';
+
+				&:last-chlid {
+					--at-apply: 'me-0 pe-0';
+				}
+
+				> div {
+					--at-apply: 'bg-black rounded-lg px-2 py-1';
+				}
+			}
+		}
+	}
+}
+</style>
