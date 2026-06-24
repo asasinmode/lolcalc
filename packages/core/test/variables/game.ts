704b7e1628414f7ad8b8928630aa079c0ff4f80d
asasinmode <skin9470@gmail.com>
2026-06-24 16:11:28 +0200

    extended equals const + single stat tests

--- packages/core/test/variables/game.ts
+++ packages/core/test/variables/game.ts
@@ -30,4 +30,21 @@ test.only('extended equals', async (t) => {
 		const icebornGauntlet = replaceGameVariables((TEXT as unknown as TText).items[ITEM_NAME_TO_ID.icebornGauntlet].tooltipShop[0]![1]!, 'item', { item: ITEMS_BY_NAME.icebornGauntlet }, undefined, { isExtended: true });
 		assertMetaSuffix('SpellbladeDamage', `<${CHAMPION_STAT_TO_SCALING_TAG.attackDamage}>150% base</${CHAMPION_STAT_TO_SCALING_TAG.attackDamage}> %i:${STAT_ICON.attackDamage}%`, icebornGauntlet);
 	});
+
+	t.test('const + single stat scaling', () => {
+		const zazzak = replaceGameVariables((TEXT as unknown as TText).items[ITEM_NAME_TO_ID.zazZakRealmspike].tooltipShop[0]![1]!, 'item', { item: ITEMS_BY_NAME.zazZakRealmspike }, undefined, { isExtended: true });
+		assertMetaSuffix('TooltipDamage', `<const>10</const> <${CHAMPION_STAT_TO_SCALING_TAG.abilityPower}>+ 15%</${CHAMPION_STAT_TO_SCALING_TAG.abilityPower}>%i:${STAT_ICON.abilityPower}%`, zazzak);
+
+		const thornmail = replaceGameVariables((TEXT as unknown as TText).items[ITEM_NAME_TO_ID.thornmail].tooltipShop[0]![1]!, 'item', { item: ITEMS_BY_NAME.thornmail }, undefined, { isExtended: true });
+		assertMetaSuffix('TotalDamage', `<const>20</const> <${CHAMPION_STAT_TO_SCALING_TAG.armor}>+ 10% bonus</${CHAMPION_STAT_TO_SCALING_TAG.armor}> %i:${STAT_ICON.armor}%`, thornmail);
+
+		const sunfireAegis = replaceGameVariables((TEXT as unknown as TText).items[ITEM_NAME_TO_ID.sunfireAegis].tooltipShop[0]![1]!, 'item', { item: ITEMS_BY_NAME.sunfireAegis }, undefined, { isExtended: true });
+		assertMetaSuffix('DPS', `<const>20</const> <${CHAMPION_STAT_TO_SCALING_TAG.hp}>+ 1% bonus</${CHAMPION_STAT_TO_SCALING_TAG.hp}> %i:${STAT_ICON.hp}%`, sunfireAegis);
+
+		const axiomArc = replaceGameVariables((TEXT as unknown as TText).items[ITEM_NAME_TO_ID.axiomArc].tooltipShop[0]![1]!, 'item', { item: ITEMS_BY_NAME.axiomArc }, undefined, { isExtended: true });
+		assertMetaSuffix('UltimateRefund', `<const>10</const> <${CHAMPION_STAT_TO_SCALING_TAG.lethality}>+ 25%</${CHAMPION_STAT_TO_SCALING_TAG.lethality}>%i:${STAT_ICON.lethality}%`, axiomArc);
+
+		const deadMansPlate = replaceGameVariables((TEXT as unknown as TText).items[ITEM_NAME_TO_ID.deadMansPlate].tooltipShop[0]![1]!, 'item', { item: ITEMS_BY_NAME.deadMansPlate }, undefined, { isExtended: true });
+		assertMetaSuffix('MaxDamageCalc', `<const>40</const> <${CHAMPION_STAT_TO_SCALING_TAG.attackDamage}>+ 100% base</${CHAMPION_STAT_TO_SCALING_TAG.attackDamage}> %i:${STAT_ICON.attackDamage}%`, deadMansPlate);
+	});
 });
