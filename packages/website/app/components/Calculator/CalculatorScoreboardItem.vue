fd938db0b064f947ff40cb8c0864e8b1d150b094
asasinmode <skin9470@gmail.com>
2026-06-30 18:18:20 +0200

    dragon soul tooltip max w

--- packages/website/app/components/Calculator/CalculatorScoreboardItem.vue
+++ packages/website/app/components/Calculator/CalculatorScoreboardItem.vue
@@ -2405,9 +2405,13 @@ defineExpose({ el });
 				}
 
 				.dragon-thing.hover-tooltip {
-					--at-apply: 'w-fit max-w-screen fixed whitespace-nowrap';
+					--at-apply: 'w-fit max-w-[min(100vw,calc(154*var(--spacing)))] fixed whitespace-nowrap';
 					top: calc(anchor(bottom));
 					position-anchor: --scoreboard-item-dragons;
+
+					> .game-description {
+						--at-apply: 'text-wrap leading-normal';
+					}
 				}
 			}
 
