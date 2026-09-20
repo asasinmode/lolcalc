ef170441a7873e57e86beab7f82ffe1d43ebe87f
asasinmode <skin9470@gmail.com>
2026-09-20 15:44:40 +0200

    more about page styles & begrudgingly conform to the boring usage of periods and capital letters

--- packages/website/app/pages/about.vue
+++ packages/website/app/pages/about.vue
@@ -6,11 +6,11 @@ import { AbilityType, ITEM_NAME_TO_ID } from '@lolcalc/shared/index';
 
 const { reportAnIssue } = useReportIssueDialog();
 
-const discrepancyMaxHealthMana = useSimpleDescription('about-discrepancy-1', `max <scalehealth>%i:${STAT_ICON.hp}%health</scalehealth> and <scalemana>%i:${STAT_ICON.mana}%mana</scalemana>/<scaleenergy>ability resource</scaleenergy> have a <span class="error-margin">margin of error of <strong>1</strong></span>. Most of the differences should be coming from floating point arithmetic and the in game ui rounding the displayed <scalehealth>hp</scalehealth> up and <scalemana>mana</scalemana> down. This means that the game displays something like <span class="code-like">2773.0000001</span> as <span class="code-like">2774</span>, even though it's effectively <span class="code-like">2773</span> ([example config](#TODO))`);
-const discrepancyAttackRangeRfc = useSimpleDescription('about-discrepancy-2', `%i:${STAT_ICON.attackRange}%attack range with [${simpleFormattingGameAbilityImage(GameAbilityId.build(AbilityType.item, ITEM_NAME_TO_ID.rfc))}Rapid Firecannon's](https://wiki.leagueoflegends.com/en-us/Rapid_Firecannon) passive active has a <span class="error-margin">margin of error of <strong>1</strong></span>. I don't know why ([config with chogath with R 6 stacks/rakan from test](#TODO))`);
-const discrepancyBloodmailRetribution = useSimpleDescription('about-discrepancy-3', `<scalead>%i:${STAT_ICON.attackDamage}%attack damage</scalead> with [${simpleFormattingGameAbilityImage(GameAbilityId.build(AbilityType.item, ITEM_NAME_TO_ID.overlordsBloodmail))}Overlord's Bloodmail's](https://wiki.leagueoflegends.com/en-us/Overlord's_Bloodmail) [Retribution](https://wiki.leagueoflegends.com/en-us/Named_item_effect#Retribution) passive can be slightly off for high <scalehealth>max hp</scalehealth> values, or might be difficult to exactly verify with the game, since it calculates it &quot;on top&quot; of other stat calculations (as if it was an external effect). For example, if in game you see a champion with <scalehealth>300 %i:${STAT_ICON.hp}%hp</scalehealth>, in that exact moment you are looking at their stats, their displayed <scalead>ad</scalead> might be lagging behind and showing the value for <scalehealth>299 %i:${STAT_ICON.hp}%hp</scalehealth> that was just there, before it regenerated. Because of this, when [Retribution](https://wiki.leagueoflegends.com/en-us/Named_item_effect#Retribution) is being calculated, <scalead>%i:${STAT_ICON.attackDamage}%attack damage's</scalead> <span class="error-margin">margin of error is <strong>1</strong> within <strong>+/-0.5%</strong></span> <scalehealth>%i:${STAT_ICON.hp}%current health</scalehealth> (check if <scalead>ad</scalead> is correct within +/-0.5% <scalehealth>hp</scalehealth>)`);
+const discrepancyMaxHealthMana = useSimpleDescription('about-discrepancy-1', `Max <scalehealth>%i:${STAT_ICON.hp}%health</scalehealth> and <scalemana>%i:${STAT_ICON.mana}%mana</scalemana>/<scaleenergy>ability resource</scaleenergy> have a <span class="error-margin">margin of error of <strong>1</strong></span>. Most of the differences should be coming from floating point arithmetic and the in game ui rounding the displayed <scalehealth>hp</scalehealth> up and <scalemana>mana</scalemana> down. This means that the game displays something like <span class="code-like">2773.0000001</span> as <span class="code-like">2774</span>, even though it's effectively <span class="code-like">2773</span> ([example config](#TODO)).`);
+const discrepancyAttackRangeRfc = useSimpleDescription('about-discrepancy-2', `%i:${STAT_ICON.attackRange}%Attack range with [${simpleFormattingGameAbilityImage(GameAbilityId.build(AbilityType.item, ITEM_NAME_TO_ID.rfc))}Rapid Firecannon's](https://wiki.leagueoflegends.com/en-us/Rapid_Firecannon) passive active has a <span class="error-margin">margin of error of <strong>1</strong></span>. I don't know why ([config with chogath with R 6 stacks/rakan from test](#TODO)).`);
+const discrepancyBloodmailRetribution = useSimpleDescription('about-discrepancy-3', `<scalead>%i:${STAT_ICON.attackDamage}%Attack damage</scalead> with [${simpleFormattingGameAbilityImage(GameAbilityId.build(AbilityType.item, ITEM_NAME_TO_ID.overlordsBloodmail))}Overlord's Bloodmail's](https://wiki.leagueoflegends.com/en-us/Overlord's_Bloodmail) [Retribution](https://wiki.leagueoflegends.com/en-us/Named_item_effect#Retribution) passive can be slightly off for high <scalehealth>max hp</scalehealth> values, or might be difficult to exactly verify with the game, since it calculates it &quot;on top&quot; of other stat calculations (as if it was an external effect). For example, if in game you see a champion with <scalehealth>300 %i:${STAT_ICON.hp}%hp</scalehealth>, in that exact moment you are looking at their stats, their displayed <scalead>ad</scalead> might be lagging behind and showing the value for <scalehealth>299 %i:${STAT_ICON.hp}%hp</scalehealth> that was just there, before it regenerated. Because of this, when [Retribution](https://wiki.leagueoflegends.com/en-us/Named_item_effect#Retribution) is a factor, <scalead>%i:${STAT_ICON.attackDamage}%attack damage's</scalead> <span class="error-margin">margin of error is <strong>1</strong> within <strong>+/-0.5%</strong></span> <scalehealth>%i:${STAT_ICON.hp}%current health</scalehealth> (check if <scalead>ad</scalead> is correct within +/-0.5% <scalehealth>hp</scalehealth>).`);
 
-const nonPassiveAbilitiesDisclaimer = useSimpleDescription('about-non-passive-abilities-disclaimer', `non-passive abilities (like [${simpleFormattingGameAbilityImage(GameAbilityId.build(AbilityType.champion, 'Ambessa', 'r', 0))}Ambessa R passive](https://wiki.leagueoflegends.com/en-us/Ambessa#Public_Execution) or [${simpleFormattingGameAbilityImage(GameAbilityId.build(AbilityType.champion, 'Nocturne', 'w', 0))}Nocturne W passive](https://wiki.leagueoflegends.com/en-us/Nocturne#Shroud_of_Darkness) and [rune paths](https://wiki.leagueoflegends.com/en-us/Rune#Rune_paths) <span class="error-margin">are not implemented</span> <i>(yet)</i>. See the [champion/item/rune setup](/guide#test-setup) recommended for verifying the calculations yourself`);
+const nonPassiveAbilitiesDisclaimer = useSimpleDescription('about-non-passive-abilities-disclaimer', `Non-passive abilities (like [${simpleFormattingGameAbilityImage(GameAbilityId.build(AbilityType.champion, 'Ambessa', 'r', 0))}Ambessa R passive](https://wiki.leagueoflegends.com/en-us/Ambessa#Public_Execution) or [${simpleFormattingGameAbilityImage(GameAbilityId.build(AbilityType.champion, 'Nocturne', 'w', 0))}Nocturne W passive](https://wiki.leagueoflegends.com/en-us/Nocturne#Shroud_of_Darkness) and [rune paths](https://wiki.leagueoflegends.com/en-us/Rune#Rune_paths) <span class="error-margin">are not implemented</span> <i>(yet)</i>. See the [champion/item/rune setup](/guide#test-setup) recommended for verifying the calculations yourself.`);
 </script>
 
 <template>
@@ -23,7 +23,9 @@ const nonPassiveAbilitiesDisclaimer = useSimpleDescription('about-non-passive-ab
 			</NuxtLink>
 		</p>
 
-		<h2>what is it?</h2>
+		<h2>
+			what is it?
+		</h2>
 		<p>
 			<strong>lolcalc</strong> is intended to be a fully fledged, all-included <a href="https://www.leagueoflegends.com/" target="_blank">League of Legends</a> damage calculator (WIP). The goal is to show the same stats and damage numbers the game does for any champion/item/rune/buff/debuff combination.
 		</p>
@@ -32,14 +34,14 @@ const nonPassiveAbilitiesDisclaimer = useSimpleDescription('about-non-passive-ab
 			does it work?
 		</h2>
 		<p>
-			while it's impossible for me to check all 172+ champions with every item/rune/dragon/quest/buff/debuff combination, I tried my best to make it work and <NuxtLink to="/guide#guide-examples"> here are some examples of it </NuxtLink>. If you encounter a configuration that's not calculated correctly (<a href="#known-discrepancies">known discrepancies</a>), please <button class="link-like" @click="reportAnIssue">report it</button>
+			While it's impossible for me to check all 172+ champions with every item/rune/dragon/quest/buff/debuff combination, I tried my best to make it work and <NuxtLink to="/guide#guide-examples"> here are some examples of it </NuxtLink>. If you encounter a configuration that's not calculated correctly (<a href="#known-discrepancies">known discrepancies</a>), please <button class="link-like" @click="reportAnIssue">report it</button>
 		</p>
 		<p class="alert info">
-			for now only champion <u>passives</u> (not QWER), items, and rune <u>shards</u> (not paths) calculate properly
+			For now only champion <u>passives</u> (not QWER), items, and rune <u>shards</u> (not paths) calculate properly.
 			<Icon class="i-ph:info" />
 		</p>
 		<p class="game-description" v-html="nonPassiveAbilitiesDisclaimer" />
-		<p>for when these and other features will be implemented, check <a href="#TODO">the roadmap</a></p>
+		<p>For when these and other features will be implemented, check <a href="#TODO">the roadmap</a>.</p>
 
 		<h2 id="known-discrepancies">currently known calculation discrepancies</h2>
 		<ul>
@@ -75,18 +77,34 @@ const nonPassiveAbilitiesDisclaimer = useSimpleDescription('about-non-passive-ab
 @layer page {
 	#about,
 	#guide {
-		--at-apply: 'text-lg leading-8 max-inline-216 mx-auto pbs-[--fluid-12-32] text-neutral-200';
+		--at-apply: 'text-lg leading-8 max-inline-216 mx-auto pbs-[--fluid-12-32] mbe-[--fluid-28-40] text-neutral-200';
 
 		> h1 {
 			--at-apply: 'text-[length:--fluid-24-32] font-700 mbe-[0.75em] text-center text-white';
 		}
 
 		> h2 {
-			--at-apply: 'text-[length:--fluid-22-28] font-700 mbs-[1em] mbe-[0.66em] text-white';
+			--at-apply: 'text-[length:--fluid-22-28] font-700 mbs-[1em] mbe-[0.5em] text-white';
 		}
 
 		.code-like {
 			--at-apply: 'font-mono text-white bg-slate-900 px-1 py-0.5 -mx-0.5 -my-0.5 rounded-sm';
+		}
+
+		> ul {
+			--at-apply: 'list-disc';
+
+			> li {
+				--at-apply: 'ms-5 my-[0.5em]';
+			}
+		}
+
+		> p {
+			--at-apply: 'my-[1em]';
+		}
+
+		> h2 + p {
+			--at-apply: 'mbs-0';
 		}
 
 		a,
@@ -96,6 +114,10 @@ const nonPassiveAbilitiesDisclaimer = useSimpleDescription('about-non-passive-ab
 			&:hover {
 				--at-apply: 'underline';
 			}
+		}
+
+		.alert {
+			--at-apply: 'my-6';
 		}
 
 		.error-margin {
@@ -109,6 +131,10 @@ const nonPassiveAbilitiesDisclaimer = useSimpleDescription('about-non-passive-ab
 	#guide {
 		img {
 			--at-apply: 'inline-block size-5 me-[0.5ch] -translate-y-px';
+		}
+
+		.game-description {
+			--at-apply: 'text-neutral-200';
 		}
 	}
 }
