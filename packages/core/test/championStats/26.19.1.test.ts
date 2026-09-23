f18b8ddbc6644bb3e0b76a0b04da25c704c09a4d
asasinmode <skin9470@gmail.com>
2026-09-23 19:32:25 +0200

    make jayce test better

--- packages/core/test/championStats/26.19.1.test.ts
+++ packages/core/test/championStats/26.19.1.test.ts
@@ -10,8 +10,7 @@ test.before(() => {
 	setupPatchFixture(fixture);
 });
 
-test.only('26.19 Jayce', async (t) => {
-	t.runOnly(true);
+test('26.19 Jayce', async (t) => {
 	const sourceCommon: IOverrides<'Jayce'> = {
 		level: 18,
 		runes: {
@@ -21,102 +20,158 @@ test.only('26.19 Jayce', async (t) => {
 				defensive: 'health',
 			},
 		},
-		items: [ITEMS_BY_NAME.jakSho, ITEMS_BY_NAME.overlordsBloodmail, ITEMS_BY_NAME.thornmail, ITEMS_BY_NAME.forceOfNature, ITEMS_BY_NAME.bloodthirster, ITEMS_BY_NAME.endlessHunger],
+		items: [ITEMS_BY_NAME.jakSho, ITEMS_BY_NAME.overlordsBloodmail, ITEMS_BY_NAME.thornmail, ITEMS_BY_NAME.kaenicRookern, ITEMS_BY_NAME.bloodthirster, ITEMS_BY_NAME.endlessHunger],
 	};
 
 	await t.test('base', async () => {
 		const damageSource = await setupDamageSource(fixture, 'Jayce', sourceCommon);
 
 		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
-			armor: 152,
-			magicResist: 97,
-		}, damageSource);
+			armor: 227,
+			magicResist: 177,
+		}, damageSource, 'cannon');
 
 		forceShapeshift(damageSource, 1);
 		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
-			armor: 182,
-			magicResist: 127,
-		}, damageSource);
+			armor: 269,
+			magicResist: 219,
+		}, damageSource, 'hammer');
 
 		(damageSource.internalItemData.value as IInternalItemDataOf<'jakSho'>).vbResistance = 1;
 		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
-			armor: 205,
-			magicResist: 150,
-		}, damageSource);
+			armor: 318,
+			magicResist: 269,
+		}, damageSource, 'hammer jaksho');
 
 		forceShapeshift(damageSource, 0);
 		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
-			armor: 166,
-			magicResist: 111,
-		}, damageSource);
+			armor: 263,
+			magicResist: 215,
+		}, damageSource, 'cannon jaksho');
 
 		forceShapeshift(damageSource, 1);
 		(damageSource.internalItemData.value as IInternalItemDataOf<'jakSho'>).vbResistance = 0;
-		damageSource.currentHealth.value = 550;
+		damageSource.currentHealth.value = 490;
 		await nextTick();
 
 		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
-			armor: 183,
-			magicResist: 128,
-		}, damageSource);
+			attackDamage: 398,
+			armor: 271,
+			magicResist: 221,
+		}, damageSource, 'hammer bloodmail');
 
 		(damageSource.internalItemData.value as IInternalItemDataOf<'jakSho'>).vbResistance = 1;
-		damageSource.currentHealth.value = 562;
+		damageSource.currentHealth.value = 544;
 		await nextTick();
 
 		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
-			armor: 206,
-			magicResist: 151,
-		}, damageSource);
+			attackDamage: 398,
+			armor: 320,
+			magicResist: 272,
+		}, damageSource, 'hammer jaksho bloodmail');
 	});
 
-	await t.test('2 infernals 2 mountains', { only: true }, async () => {
+	await t.test('2 infernals 2 mountains', async () => {
 		const damageSource = await setupDamageSource(fixture, 'Jayce', {
 			...sourceCommon,
 			dragonStacks: ['Infernal', 'Infernal', 'Mountain', 'Mountain'],
 		});
 
 		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
-			armor: 167,
-			magicResist: 107,
-		}, damageSource);
+			armor: 250,
+			magicResist: 195,
+		}, damageSource, 'cannon');
 
 		forceShapeshift(damageSource, 1);
 		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
-			armor: 201,
-			magicResist: 140,
-		}, damageSource);
+			armor: 297,
+			magicResist: 242,
+		}, damageSource, 'hammer');
 
 		(damageSource.internalItemData.value as IInternalItemDataOf<'jakSho'>).vbResistance = 1;
+		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
+			armor: 351,
+			magicResist: 298,
+		}, damageSource, 'hammer jaksho');
+
 		forceShapeshift(damageSource, 0);
 		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
-			armor: 182,
-			magicResist: 122,
-		}, damageSource);
-
-		forceShapeshift(damageSource, 1);
-		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
-			armor: 226,
-			magicResist: 165,
-		}, damageSource);
+			armor: 289,
+			magicResist: 236,
+		}, damageSource, 'cannon jaksho');
 
 		forceShapeshift(damageSource, 1);
 		(damageSource.internalItemData.value as IInternalItemDataOf<'jakSho'>).vbResistance = 0;
-		damageSource.currentHealth.value = 575;
+		damageSource.currentHealth.value = 218;
 		await nextTick();
 
 		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
-			armor: 201,
-			magicResist: 141,
-		}, damageSource);
+			attackDamage: 419,
+			armor: 299,
+			magicResist: 245,
+		}, damageSource, 'hammer bloodmail');
 
 		(damageSource.internalItemData.value as IInternalItemDataOf<'jakSho'>).vbResistance = 1;
-		damageSource.currentHealth.value = 595;
+		damageSource.currentHealth.value = 544;
 		await nextTick();
 
 		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
-			armor: 227,
-			magicResist: 166,
-		}, damageSource);
+			attackDamage: 398,
+			armor: 354,
+			magicResist: 301,
+		}, damageSource, 'hammer jaksho bloodmail');
+	});
+
+	/* nothing other than AD changes, jayce's R doesn't factor in mid quest AD */
+	await t.test('2 infernals 2 mountains | mid quest', async () => {
+		const damageSource = await setupDamageSource(fixture, 'Jayce', {
+			...sourceCommon,
+			dragonStacks: ['Infernal', 'Infernal', 'Mountain', 'Mountain'],
+			roleQuest: 'mid',
+		});
+
+		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
+			armor: 250,
+			magicResist: 195,
+		}, damageSource, 'cannon');
+
+		forceShapeshift(damageSource, 1);
+		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
+			armor: 297,
+			magicResist: 242,
+		}, damageSource, 'hammer');
+
+		(damageSource.internalItemData.value as IInternalItemDataOf<'jakSho'>).vbResistance = 1;
+		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
+			armor: 351,
+			magicResist: 298,
+		}, damageSource, 'hammer jaksho');
+
+		forceShapeshift(damageSource, 0);
+		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
+			armor: 289,
+			magicResist: 236,
+		}, damageSource, 'cannon jaksho');
+
+		forceShapeshift(damageSource, 1);
+		(damageSource.internalItemData.value as IInternalItemDataOf<'jakSho'>).vbResistance = 0;
+		damageSource.currentHealth.value = 228;
+		await nextTick();
+
+		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
+			attackDamage: 440,
+			armor: 299,
+			magicResist: 245,
+		}, damageSource, 'hammer bloodmail');
+
+		(damageSource.internalItemData.value as IInternalItemDataOf<'jakSho'>).vbResistance = 1;
+		damageSource.currentHealth.value = 481;
+		await nextTick();
+
+		typedPartialDeepStrictEqual(damageSource.computed.formattedStatTotals.value, {
+			attackDamage: 440,
+			armor: 354,
+			magicResist: 301,
+		}, damageSource, 'hammer jaksho bloodmail');
 	});
 });
